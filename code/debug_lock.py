2cfd67eb1c792b8ebf6c3ac4a5e8b5b92d619739
Denis Beurivé <dbeurive@stamus-networks.com>
2021-05-15 13:06:53 +0200

    update

--- code/debug_lock.py
+++ code/debug_lock.py
@@ -3,24 +3,55 @@ from threading import Lock, RLock
 
 
 class BaseLock(object):
+    """
+    Base class for all extended locks.
+    """
 
     def __init__(self, in_lock: Union[Lock, RLock], in_resource: Optional[str] = None):
+        """
+        Create a new instance of BaseLock.
+
+        :param in_lock: the lock, which is an instance of threading.Lock or threading.RLock.
+        :param in_resource: the name of the lock protected resource.
+        """
         self.__locker: Optional[str] = None
         self.__resource: Optional[str] = in_resource
         self._lock: Union[Lock, RLock] = in_lock
 
     @property
     def locker(self) -> Optional[str]:
+        """
+        The identifier of the entity that acquires the lock.
+        """
         return self.__locker
 
     @property
     def resource(self) -> Optional[str]:
+        """
+        The name of the lock protected resource.
+        """
         return self.__resource
 
-    def set(self, locker: str, value: Optional[str] = None):
+    def set(self, locker: str, resource: Optional[str] = None):
+        """
+        Set the identifier of the entity that acquires the lock, and,
+        optionally, the name of the lock protected resource.
+
+        Typical usage:
+
+            resource = {}
+            lock = ExtLock("protected resource")
+            with lock.set("main"):
+                resource[1] = 2
+
+        :param locker: the identifier of the entity that acquires the lock.
+        :param resource: the name of the lock protected resource.
+        :return: the instance of BaseLock.
+        """
+
         self.__locker = locker
-        if value is not None:
-            self.__resource = value
+        if resource is not None:
+            self.__resource = resource
         return self
 
     def acquire(self):
@@ -52,16 +83,3 @@ class ExtRLock(BaseLock):
 
     def __init__(self, in_resource: Optional[str] = None):
         super().__init__(RLock(), in_resource)
-
-
-resource1 = {}
-resource2 = {}
-
-
-lock = ExtLock("resource1")
-with lock.set("main"):
-    resource1[1] = 'a'
-
-rlock = ExtRLock()
-with rlock.set("main", "resource2"):
-    resource2[1] = 'a'
